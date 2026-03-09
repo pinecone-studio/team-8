f0afd88c6a170cecb94f83ca13bf1dfddfb4ebdc
Bolormaa <bolorboloroo1127@gmail.com>
2026-03-09 18:27:00 +0800

    added url

--- frontend/src/lib/apollo-client.ts
+++ frontend/src/lib/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const httpLink = new HttpLink({
-  uri: process.env.NEXT_PUBLIC_GRAPHQL_URL || "http://localhost:8787",
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_URL || "https://team8-api.team8pinequest.workers.dev/",
 });
 
 export const apolloClient = new ApolloClient({
