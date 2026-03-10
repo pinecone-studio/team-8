24d87452018da61d48eeaeba7234e17b631fd49d
25LP5135 25LP5135 <25LP5135@G302-CC0089.local>
2026-03-10 17:18:46 +0800

    pushh

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
 import { ApolloWrapper } from "@/lib/apollo-provider";
 import "./globals.css";
-import { Geist } from "next/font/google";
+import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Team 8",
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={cn("font-sans", geist.variable)}>
+    <html lang="en" className={cn("font-sans", inter.variable)}>
       <body>
         <ApolloWrapper>{children}</ApolloWrapper>
       </body>
