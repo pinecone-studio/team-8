261ccd46f3914e06fd65440728818d31dfed3bb0
25lp7998 User <25lp7998@iMac-302-1F.local>
2026-03-10 15:43:02 +0800

    update main

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { ApolloWrapper } from "@/lib/apollo-provider";
 import "./globals.css";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 export const metadata: Metadata = {
   title: "Team 8",
@@ -13,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", geist.variable)}>
       <body>
         <ApolloWrapper>{children}</ApolloWrapper>
       </body>
